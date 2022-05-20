2da5e6cbdffad407493d45b4f95cc3842cd08412
unknown <2598030746@qq.com>
2022-05-20 23:33:50 +0800

    feat：accountDetail页面初步布局

--- src/screens/accountDetail/index.tsx
+++ src/screens/accountDetail/index.tsx
@@ -1,5 +1,22 @@
+import styled from "@emotion/styled";
+import { Button, Card } from "antd";
+import { ScreenContainer } from "../../components/lib"
+import { useAuth } from "../../context/auth-context"
+
 export const AccountDetailScreen = () => {
-  return <>
-    AccountDetailScreen
-  </>
-} 
+  // const { user } = useAuth();
+
+
+  return <ScreenContainer>
+    {/* {user?.data.userInfo.authorityId} */}
+    <UserDetailContainer title="用户信息" extra={<Button type={'link'} href="#">修改信息</Button>} style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </UserDetailContainer>
+  </ScreenContainer>
+}
+
+const UserDetailContainer = styled(Card)`
+  width: 100%;
+`
